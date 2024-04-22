28d95e4cde8288a79685006204c6505f47cf5a42
Kareem Ghazi <k.ghazi2005@gmail.com>
2024-04-22 21:57:18 +0200

    Add functionality to "Add" & "Remove button.

--- main.cpp
+++ main.cpp
@@ -16,15 +16,16 @@
 #define WINDOW1_NAME "Camera"
 #define WINDOW2_NAME "Settings & Information"
 #define WINDOW3_NAME "Add a person"
+#define WINDOW4_NAME "Remove a person"
 
 using namespace cv;
 using namespace std;
 
-void createGUI(Mat& originalFrame, VideoCapture& capture, int faceCount);
+bool createGUI(Mat& originalFrame, VideoCapture& capture, int faceCount);
 void createInformationComponent(Mat& frame, int faceCount);
 void createSettingsComponent(Mat& frame, VideoCapture& capture);
-void addEntry();
-void removeEntry();
+bool addEntry();
+bool removeEntry();
 void openWindow(const cv::String& name);
 void closeWindow(const cv::String& name);
 
@@ -34,20 +35,20 @@ Image imageGray;
 Image imageHist;
 vector<Rect> faces;
 ObjectDetector faceDetector("Resources/haarcascade_frontalface_default.xml");
-Mat addEntryGUI;
+
 bool webcamOn = true;
 bool fpsCounter = false;
-bool isAdding = false;
-bool isRemoving = false;
 int faceCounter = 1;
 double fps = 0;
-bool isRunning = true;
+bool isAdding = false;
+bool isRemoving = false;
 
 int main() {
 	VideoCapture cap(0, CAP_DSHOW);
 
 	const cv::String windows[] = { WINDOW1_NAME, WINDOW2_NAME };
 	cvui::init(windows, 2);
+	bool isRunning = true;
 
 	while (cv::getWindowProperty(WINDOW2_NAME, 0) >= 0 && cv::getWindowProperty(WINDOW1_NAME, 0) >= 0)
 	{
@@ -56,7 +57,7 @@ int main() {
 		if (!cap.isOpened())
 		{
 			cvui::context(WINDOW2_NAME);
-			createGUI(GUIFrame, cap, 0);
+			isRunning = createGUI(GUIFrame, cap, 0);
 
 			cvui::imshow(WINDOW2_NAME, GUIFrame);
 
@@ -98,25 +99,6 @@ int main() {
 		cvui::imshow(WINDOW1_NAME, image.getImageMatrix());
 		cvui::imshow(WINDOW2_NAME, GUIFrame);
 
-		// FILTHIEST PIECE OF CODE I'VE EVER WRITTEN (yes, it's kareem's code)
-		if (isAdding) {
-			addEntry();
-
-			if (isAdding)
-			{
-				cvui::update(WINDOW3_NAME);
-				cv::imshow(WINDOW3_NAME, addEntryGUI);
-			}
-		}
-
-		/*if (isRemoving)
-		{
-
-		}
-		else {
-
-		}*/
-
 		if (cv::waitKey(1) == 27 || !isRunning) {
 			break;
 		}
@@ -128,7 +110,7 @@ int main() {
 }
 
 // Creates a graphical user interface for interacting with the system. Includes system information & settings.
-void createGUI(Mat& frame, VideoCapture& capture, int faceCount)
+bool createGUI(Mat& frame, VideoCapture& capture, int faceCount)
 {
 	Mat copyFrame = frame.clone();
 	Mat doubleBuffer = copyFrame.clone();
@@ -136,13 +118,15 @@ void createGUI(Mat& frame, VideoCapture& capture, int faceCount)
 	doubleBuffer.copyTo(frame);
 
 	if (cvui::button(frame, frame.cols - 90, frame.rows - 40, "Quit")) {
-		isRunning = false;
+		return false;
 	}
 
 	createInformationComponent(frame, faceCount);
 	createSettingsComponent(frame, capture);
 
 	cvui::printf(frame, 30, frame.rows - 31, 0.4, 0xCECECE, "Copyright 2024 by Kareem Ghazi");
+
+	return true;
 }
 
 // Creates information 'window' component inside of the specified window.
@@ -151,26 +135,26 @@ void createInformationComponent(Mat& frame, int faceCount)
 	cvui::window(frame, 20, 20, 210, 240, "Information");
 
 	cvui::beginColumn(frame, 34, 55, -1, -1, 10);
-		if (faceCount >= 1) {
-			cvui::printf(0.4, 0xcccccc, "Number of People Detected: ");
-			cvui::printf(0.4, 0x00ff00, "%d", faceCount);
-		}
-		else {
-			cvui::printf(0.4, 0xcccccc, "Number of People Detected: ");
-			cvui::printf(0.4, 0xcccccc, "%d", faceCount);
-		}
+	if (faceCount >= 1) {
+		cvui::printf(0.4, 0xcccccc, "Number of People Detected: ");
+		cvui::printf(0.4, 0x00ff00, "%d", faceCount);
+	}
+	else {
+		cvui::printf(0.4, 0xcccccc, "Number of People Detected: ");
+		cvui::printf(0.4, 0xcccccc, "%d", faceCount);
+	}
 
-		cvui::space(5);
-		cvui::printf(0.4, 0xcccccc, "Currently Detecting: ");
+	cvui::space(5);
+	cvui::printf(0.4, 0xcccccc, "Currently Detecting: ");
 
-		if (faceCount == 1)
-		{
-			cvui::printf(0.4, 0x00ff00, "Kareem Ghazi");
-		}
-		else if (faceCount >= 2) {
-			cvui::printf(0.4, 0x00ff00, "Kareem Ghazi");
-			cvui::printf(0.4, 0xff474c, "...and %d other people.", faceCount - 1);
-		}
+	if (faceCount == 1)
+	{
+		cvui::printf(0.4, 0x00ff00, "Kareem Ghazi");
+	}
+	else if (faceCount >= 2) {
+		cvui::printf(0.4, 0x00ff00, "Kareem Ghazi");
+		cvui::printf(0.4, 0xff474c, "...and %d other people.", faceCount - 1);
+	}
 	cvui::endColumn();
 }
 
@@ -178,78 +162,98 @@ void createInformationComponent(Mat& frame, int faceCount)
 void createSettingsComponent(Mat& frame, VideoCapture& capture)
 {
 	cvui::window(frame, frame.cols - 200, 20, 180, 240, "Settings");
+	cvui::checkbox(frame, frame.cols - 185, 55, "Webcam", &webcamOn);
 
-	cvui::beginColumn(frame, frame.cols - 185, 55, -1, -1, 10);
-		cvui::checkbox("Webcam", &webcamOn);
+	if (webcamOn && !capture.isOpened())
+	{
+		capture.open(0, CAP_DSHOW);
+	}
+	else if (!webcamOn && capture.isOpened())
+	{
+		capture.release();
+	}
 
-		if (webcamOn && !capture.isOpened())
-		{
-			capture.open(0, CAP_DSHOW);
-		}
-		else if (!webcamOn && capture.isOpened())
-		{
-			capture.release();
-		}
-		
-		cvui::checkbox("FPS Counter", &fpsCounter);
+	cvui::checkbox(frame, frame.cols - 185, 80, "FPS Counter", &fpsCounter);
 
-		if (fpsCounter && webcamOn)
-		{
-			ImageProcessor::putText(image, 10, 20, 'B', 0.6, to_string(fps));
-		}
-		else {
-			fpsCounter = false;
-		}
-		
-		cvui::space(7);
-		cvui::printf(0.4, 0xcccccc, "Adds a new person.");
-		if (cvui::button("Add")) {
-			/*system("cls");
-			cout << "Enter the person's name: " << endl;
-			
-			string name;
-			cin >> name;*/
+	if (fpsCounter && webcamOn)
+	{
+		ImageProcessor::putText(image, 10, 20, 'B', 0.6, to_string(fps));
+	}
+	else {
+		fpsCounter = false;
+	}
 
-			openWindow(WINDOW3_NAME);
-			addEntry();
-		}
+	cvui::printf(frame, frame.cols - 185, 125, 0.4, 0xcccccc, "Adds a new person.");
+	if (cvui::button(frame, frame.cols - 185, 145, "Add")) {
+		/*system("cls");
+		cout << "Enter the person's name: " << endl;
 
-		cvui::space(7);
-		cvui::printf(0.4, 0xcccccc, "Removes a person.");
-		if (cvui::button("Remove")) {
-			removeEntry();
-		}
-	cvui::endColumn();
-}
+		string name;
+		cin >> name;*/
 
-void addEntry()
-{
-	isAdding = true;
+		openWindow(WINDOW3_NAME);
+		isAdding = addEntry();
+	}
 
-	if (isAdding) {
-		cvui::context(WINDOW3_NAME);
+	if (isAdding)
+	{
+		isAdding = addEntry();
+	}
 
-		addEntryGUI = Mat(100, 425, CV_8UC3);
-		addEntryGUI = cv::Scalar(1, 31, 20);
+	cvui::printf(frame, frame.cols - 185, 195, 0.4, 0xcccccc, "Removes a person.");
+	if (cvui::button(frame, frame.cols - 185, 215, "Remove")) {
+		openWindow(WINDOW4_NAME);
+		isRemoving = removeEntry();
+	}
 
-		cvui::printf(addEntryGUI, 70, 20, 0.4, 0x00ff00, "Press the record button to record your face (%d/10)", faceCounter);
-
-		if (cvui::button(addEntryGUI, 110, 40, "Record")) {
-			faceCounter++;
-		}
-
-		if (faceCounter == 11)
-		{
-			closeWindow(WINDOW3_NAME);
-			faceCounter = 1;
-			isAdding = false;
-		}
+	if (isRemoving)
+	{
+		isRemoving = removeEntry();
 	}
 }
 
-void removeEntry()
+bool addEntry()
 {
+	cvui::context(WINDOW3_NAME);
 
+	Mat addEntryGUI = Mat(90, 385, CV_8UC3);
+	addEntryGUI = cv::Scalar(1, 31, 20);
+
+	cvui::printf(addEntryGUI, 25, 20, 0.4, 0x00ff00, "Press the record button to record your face. (%d/10)", faceCounter);
+
+	if (cvui::button(addEntryGUI, 145, 45, "Record")) {
+		faceCounter++;
+	}
+
+	if (faceCounter == 11)
+	{
+		closeWindow(WINDOW3_NAME);
+		faceCounter = 1;
+		return false;
+	}
+
+	cvui::imshow(WINDOW3_NAME, addEntryGUI);
+
+	return true;
+}
+
+bool removeEntry()
+{
+	cvui::context(WINDOW4_NAME);
+
+	Mat removeSuccess = Mat(90, 350, CV_8UC3);
+	removeSuccess = cv::Scalar(1, 31, 20);
+
+	cvui::printf(removeSuccess, 25, 20, 0.4, 0x00ff00, "Successfully removed person from the system!");
+
+	if (cvui::button(removeSuccess, 135, 45, "Close")) {
+		closeWindow(WINDOW4_NAME);
+		return false;
+	}
+
+	cvui::imshow(WINDOW4_NAME, removeSuccess);
+
+	return true;
 }
 
 // Open a new OpenCV window and watch it using cvui
